4a03dca5f1f9f2fad0ba2ec3b200003bd7db4c20
rajshish333 <rajshish333@gmail.com>
2023-12-22 11:25:30 +0530

    g

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 import "../Header/Header.css";
+import { useRef ,useEffect } from "react";
 
 export default function Header({expression , history , result}) {
+
+
+  const headerRef = useRef(null);
+
+  useEffect(() => {
+    if (headerRef.current) {
+      headerRef.current.scrollTop = headerRef.current.scrollHeight;
+    }
+  }, [expression, history, result]);
+
+
   return (
-    <div className="header costom_scroll" >
+    <div className="header costom_scroll" ref={headerRef}>
       <div className="header_history">
       {history.map((item, index) => (
           <p key={index}>{item}</p>
